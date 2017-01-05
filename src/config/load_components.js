d47109f0e35d55d5390e5b608acb09e781ecd908
lgqlee <xxgr02@163.com>
2017-01-05 23:56:49 +0800

    完善路由组件-2

--- src/config/load_components.js
+++ src/config/load_components.js
@@ -1,4 +1,8 @@
 
+// 动态注册compontents内的所有组件(不包含index.vue， 默认该文件为路由模板)，之后在全局无需注册即可使用
+// 格式为  目录名-文件名， 首字母大写
+// 例子1  src/components/page/header.vue, <Page-header>
+// 例子2  src/components/Hello.vue, <Hello>
 
 import Vue from 'vue'
 const requireContext = require.context('../components', true, /(?!index)\.vue$/)
