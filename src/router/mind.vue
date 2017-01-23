06bf9f3b564d4fc0a8ba59c827efff8c6f63cae2
lgqlee <xxgr02@163.com>
2017-01-23 22:02:45 +0800

    开始test.vue，实践css属性

---
+++ src/router/mind.vue
@@ -0,0 +1,48 @@
+<template lang="pug" >
+
+.body
+  el-row 
+    el-col(:span="8")
+      .rotate-wrap
+        .rotate
+          p hi
+    el-col(:span="8")
+    el-col(:span="8")
+  
+</template>
+<script>
+  
+</script>
+
+<style lang="stylus" scoped >
+
+bgc = #e5e9f2
+objbgc = deepskyblue
+
+.el-col
+  border 1px dashed royalblue
+  background-color bgc
+  padding 10px
+
+.rotate-wrap
+  height 200px
+  position relative
+  text-align center
+  
+  .rotate 
+    width 100px
+    height 50px
+    background-color objbgc
+    transform rotate(8deg) //deg即为角度
+    border-radius 10px
+    position absolute
+    top 0
+    right 0
+    left 0
+    bottom 0
+    margin auto
+
+    p
+      line-height @height // @height 会往上冒泡直到找到height
+  
+</style>
