5b6eeb619efc64ac8e3dd12a9c56e38880948460
lgqlee <xxgr02@163.com>
2017-01-30 13:41:43 +0800

    添加outline

--- src/router/mind/shadow.vue
+++ src/router/mind/shadow.vue
@@ -2,15 +2,22 @@
 <template lang="pug" >
 
 .body 
-  .shadow
+  .box
     W3-simple-shadow.ss
     p 简单投影
 
   .hr
 
-  .shadow
+  .box
     W3-deeppink-shadow.ss
     p 双重投影
+
+  .hr 
+
+  .box 
+    W3-outline.ss
+      p 可使用dashed 等属性
+    p 双层边框时更灵活的outline属性
 
 </template>
 
@@ -20,7 +27,7 @@
 
 <style lang="stylus" scoped >
 
-.shadow
+.box
   .ss
     margin 0 auto
   p
