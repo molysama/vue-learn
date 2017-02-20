1a6a9e1d26ea9d792a7a47116c9c1e63a05fbbf4
lgqlee <xxgr02@163.com>
2017-02-20 14:26:43 +0800

    vuex:  titleA -> title

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,7 +3,7 @@ export const cartProducts = state => {
   return state.cart.added.map(({id, quantity}) => {
     const product = state.products.all.find(p => p.id === id)
     return {
-      title: product.titleA,
+      title: product.title,
       price: product.price,
       quantity
     }
