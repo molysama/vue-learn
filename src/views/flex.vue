e98b671455dbf53776d91959eb49b9fea2698e8c
lgqlee <xxgr02@163.com>
2017-02-23 12:31:23 +0800

    add flex-css

--- src/views/flex.vue
+++ src/views/flex.vue
@@ -1,15 +1,14 @@
-<template>
-<ul class="flex-container">
-  <li class="flex-item">1</li>
-  <li class="flex-item">1</li>
-  <li class="flex-item">1</li>
-  <li class="flex-item">1</li>
-  <li class="flex-item">1</li>
-  <li class="flex-item">1</li>
-</ul>
+<template lang="jade" >
+
+
+.box(flex) Hello World
+
+
 </template>
 
 <scirpt>
+require('flex-css')
+
 export default {
   
 }
@@ -17,27 +16,9 @@ export default {
 
 <style lang="stylus" scoped >
 
-.flex-container 
-  padding: 0
-  margin: 0
-  list-style: none
-  
-  display: flex 
-  flex-flow: row wrap
-  justify-content: space-around  
-  
-.flex-item 
-  background: tomato
-  padding: 5px
-  width: 200px
-  height: 150px
-  margin-top: 10px
-  
-  line-height: 150px
-  color: white
-  font-weight: bold
-  font-size: 3em
-  text-align: center
-  
+.box 
+  width 150px
+  height 150px 
+  border 1px solid #ddd
 
 </style>
